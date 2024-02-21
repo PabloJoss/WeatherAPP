df722b19d02bb42beb687873dc7d8ac9285b026c
PabloJoss <josuecallado@hotmail.com>
2024-02-21 16:41:37 -0600

    final

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ImageBackground, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
 
 export default App = () => {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
-  const [weatherImage, setWeatherImage] = useState(null);
   const API_KEY = '845e816e9e9241a79c9231936241902';
 
   const getWeather = async () => {
@@ -15,7 +14,6 @@ export default App = () => {
       if (res.status === 200) {
         setWeatherData(data);
         setError(null);
-        updateWeatherImage(data.current.condition.code);
       } else {
         setError(`Error finding weather data!`);
       }
@@ -24,43 +22,43 @@ export default App = () => {
     }
   };
 
-  const updateWeatherImage = (weatherCode) => {
-    // Map weather codes to respective images
-    const weatherImages = {
-      1000: require('./assets/sunny.png'),
-      1003: require('./assets/partly_cloudy.png'), // Example for "Partly cloudy" condition
-      // Add more weather codes and respective image paths as needed
-    };
-    // Set default image if no match found
-    setWeatherImage(weatherImages[weatherCode] || require('./assets/default.png'));
-  };
-
   useEffect(() => {
     city ? getWeather() : setWeatherData(null);
   }, [city]);
 
+  const renderBackgroundImage = () => {
+    if (!weatherData) return require('./assets/img/default-background.jpg'); // Imagen de fondo predeterminada cuando no hay datos
+    const condition = weatherData.current.condition.text.toLowerCase();
+    if (condition.includes('sunny')) return require('./assets/img/sunny-background.jpg');
+    if (condition.includes('rain')) return require('./assets/img/rainy-background.jpg');
+    if (condition.includes('cloudy')) return require('./assets/img/cloudy-background.jpg');
+    return require('./assets/img/default-background.jpg'); // Imagen de fondo predeterminada para cualquier otra condición
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Weather App!</Text>
-      <TextInput
-        style={styles.textInput}
-        placeholder='Enter city name'
-        value={city}
-        onChangeText={(text) => setCity(text)}
-      />
-      <TouchableOpacity style={styles.button} onPress={getWeather}>
-        <Text>Get Weather</Text>
-      </TouchableOpacity>
-      {error && <Text style={styles.error}>{error}</Text>}
-      {weatherData && (
-        <ScrollView contentContainerStyle={[styles.weatherContainer, { backgroundImage: `url(${weatherImage})` }]}>
-          <Text style={styles.weatherText}>City: {weatherData.location.name}</Text>
-          <Text style={styles.weatherText}>Country: {weatherData.location.country}</Text>
-          <Text style={styles.weatherText}>Temperature: {weatherData.current.temp_c}°C</Text>
-          <Text style={styles.weatherText}>Condition: {weatherData.current.condition.text}</Text>
-        </ScrollView>
-      )}
-    </View>
+    <ImageBackground source={renderBackgroundImage()} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to Weather App!</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Enter city name'
+          value={city}
+          onChangeText={(text) => setCity(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={getWeather}>
+          <Text style={styles.buttonText}>Get Weather</Text>
+        </TouchableOpacity>
+        {error && <Text>{error}</Text>}
+        {weatherData && (
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataText}>City: {weatherData.location.name}</Text>
+            <Text style={styles.dataText}>Country: {weatherData.location.country}</Text>
+            <Text style={styles.dataText}>Temperature: {weatherData.current.temp_c}</Text>
+            <Text style={styles.dataText}>Condition: {weatherData.current.condition.text}</Text>
+          </View>
+        )}
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -69,43 +67,44 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-    padding: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
     marginBottom: 20,
+    fontWeight: 'bold',
   },
   textInput: {
     borderWidth: 1,
     borderColor: 'gray',
     width: '80%',
     padding: 10,
-    marginVertical: 10,
+    margin: 20,
     borderRadius: 5,
     backgroundColor: 'white',
   },
   button: {
-    backgroundColor: 'lightblue',
+    backgroundColor: '#9AD0C2',
     padding: 10,
     borderRadius: 5,
+  },
+  buttonText: {
+    color: 'black',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'center',
     alignItems: 'center',
-    width: '80%',
-    marginVertical: 10,
   },
-  error: {
-    color: 'red',
-    marginTop: 10,
-  },
-  weatherContainer: {
+  dataContainer: {
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20,
-    padding: 20,
-    backgroundColor: 'transparent', // Make background transparent to see the image
+    paddingTop: 10,
   },
-  weatherText: {
-    fontSize: 18,
-    marginBottom: 10,
-    color: 'white', // Set text color to contrast with background image
+  dataText: {
+    fontSize: 20,
+    color: 'white',
   },
 });
